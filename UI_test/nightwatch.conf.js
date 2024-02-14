93bb411de0fcc0b4ae4da156d825769b07bc26af
Ayatelrahman Elsayed <aya.n.elsayed@gmail.com>
2024-02-14 20:53:48 +0200

    Update nightwatch.conf.js

--- UI_test/nightwatch.conf.js
+++ UI_test/nightwatch.conf.js
@@ -12,83 +12,53 @@
 //            |___/
 
 module.exports = {
-  // An array of folders (excluding subfolders) where your tests are located;
-  // if this is not specified, the test source must be passed as the second argument to the test runner.
   src_folders: ['tests'],
-
-  // See https://nightwatchjs.org/guide/concepts/page-object-model.html
   page_objects_path: ['nightwatch/page-objects'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
   custom_commands_path: ['nightwatch/custom-commands'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
   custom_assertions_path: ['nightwatch/custom-assertions'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   plugins: [],
-  
-  // See https://nightwatchjs.org/guide/concepts/test-globals.html
   globals_path: '',
-  
   webdriver: {},
-
   test_workers: {
-    enabled: true
+    enabled: true,
   },
-
   test_settings: {
     default: {
       skip_testcases_on_fail: false,
-
       disable_error_log: false,
       launch_url: 'http://automationpractice.multiformis.com/index.php',
-
       screenshots: {
         enabled: false,
         path: 'screens',
         on_failure: true
       },
-
-      desiredCapabilities: {
-        browserName: 'chrome'
-      },
-      
-      webdriver: {
-        start_process: true,
-        server_path: require('chromedriver').path
-      },
-      
-    },
-    
-    chrome: {
-      skip_testcases_on_fail: false,
-
       desiredCapabilities: {
         browserName: 'chrome',
-        'goog:chromeOptions': {
-          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
-          //
-          // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
-          w3c: true,
-          args: [
-            '--no-sandbox',
-            '--ignore-certificate-errors',
-            '--allow-insecure-localhost',
-            '--headless'
-          ]
-        }
       },
-
       webdriver: {
         start_process: true,
         server_path: require('chromedriver').path,
-        cli_args: [
-          // --verbose
-        ]
-      }
+      },
     },
-    
+    chrome: {
+      skip_testcases_on_fail: false,
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          args: [
+            '--headless',
+            '--no-sandbox',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--window-size=1920,1080',
+          ],
+        },
+      },
+      webdriver: {
+        start_process: true,
+        server_path: require('chromedriver').path,
+      },
+    },
   },
-  
 };
+
